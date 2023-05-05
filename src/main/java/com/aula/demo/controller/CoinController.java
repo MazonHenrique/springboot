28d31d9339f96c2e32c317d48166373dca630b10
Henrique Mazon <hmazon87@yahoo.com>
2023-05-05 15:04:04 -0300

    Update aprendizado de hoje

--- src/main/java/com/aula/demo/controller/CoinController.java
+++ src/main/java/com/aula/demo/controller/CoinController.java
@@ -1,10 +1,14 @@
 package com.aula.demo.controller;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +23,53 @@ public class CoinController {
 
 	@Autowired
 	private CoinRepository coinRepository;
+	
+	@Bean
+	public Coin init() {
+		Coin c1 = new Coin();
+		c1.setName("BITCOIN");
+		c1.setPrice(new BigDecimal(100));
+		c1.setQuantity(new BigDecimal(0.004));
+		c1.setDateTime(new Timestamp(System.currentTimeMillis()));
+		
+		Coin c2 = new Coin();
+		c2.setName("BITCOIN");
+		c2.setPrice(new BigDecimal(150));
+		c2.setQuantity(new BigDecimal(0.008));
+		c2.setDateTime(new Timestamp(System.currentTimeMillis()));
+		
+		Coin c3 = new Coin();
+		c3.setName("ETHEREUM");
+		c3.setPrice(new BigDecimal(90));
+		c3.setQuantity(new BigDecimal(0.009));
+		c3.setDateTime(new Timestamp(System.currentTimeMillis()));
+		
+		Coin c4 = new Coin();
+		c3.setName("ETHEREUM");
+		c3.setPrice(new BigDecimal(90));
+		c3.setQuantity(new BigDecimal(0.012));
+		c3.setDateTime(new Timestamp(System.currentTimeMillis()));
+		
+		coinRepository.insert(c1);
+		coinRepository.insert(c2);
+		coinRepository.insert(c3);
+		coinRepository.insert(c4);
+		return c1; 
+	}
+	
+	@GetMapping
+	public ResponseEntity get() {
+		return new ResponseEntity<>(coinRepository.getALL(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{name}")
+	public ResponseEntity get(@PathVariable String name) {
+		try {
+			return new ResponseEntity<>(coinRepository.getByName(name), HttpStatus.OK);
+		}catch (Exception error) {
+			return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 	@PostMapping()
 	public ResponseEntity post(@RequestBody Coin coin) {
--- src/main/java/com/aula/demo/repository/CoinRepository.java
+++ src/main/java/com/aula/demo/repository/CoinRepository.java
@@ -1,16 +1,28 @@
 package com.aula.demo.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.aula.demo.entity.Coin;
+
+import dto.CoinDTO;
 
 @Repository
 @EnableAutoConfiguration
 public class CoinRepository {
 	
 	private static String INSERT = "insert into coin(name, price, quantity, datetime) values(?,?,?,?)";
+	
+	private static String SELECT_ALL = "select name, sum(quantity) as quantity from coin group by name";
+	
+	private static String SELCT_BY_NAME = "select * from coin where name = ?";
+	
 	private JdbcTemplate jdbcTemplate;
 	
 	public CoinRepository(JdbcTemplate jdbcTemplate) {
@@ -26,5 +38,34 @@ public class CoinRepository {
 		};
 		jdbcTemplate.update(INSERT, attr);
 		return coin;
+	}
+	
+	public List<CoinDTO> getALL(){
+		return jdbcTemplate.query(SELECT_ALL, new RowMapper<CoinDTO>(){
+			@Override
+			public CoinDTO mapRow (ResultSet rs, int rowNum) throws SQLException{
+				CoinDTO coin = new CoinDTO();
+				coin.setName(rs.getString("name"));
+				coin.setQuantity(rs.getBigDecimal("quantity"));
+				return coin;
+			}
+		});
+	}
+	
+	public List<Coin> getByName(String name){
+		Object[] attr = new Object[] {name}; 
+		return jdbcTemplate.query(SELCT_BY_NAME, new RowMapper<Coin>(){
+			@Override
+			public Coin mapRow (ResultSet rs, int rowNum) throws SQLException{
+				Coin coin = new Coin();
+				coin.setId(rs.getInt("id"));
+				coin.setName(rs.getString("name"));
+				coin.setPrice(rs.getBigDecimal("price"));
+				coin.setQuantity(rs.getBigDecimal("quantity"));
+				coin.setDateTime(rs.getTimestamp("datetime"));
+				
+				return coin;
+			}
+		}, attr);
 	}
 }
